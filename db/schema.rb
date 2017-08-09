dc46f72085903b0f0b997f176188e22f91c2e418
Cory Nistler <c.nistler3@yahoo.com>
2017-08-09 02:59:07 +0000

    made holdings migrating/model/controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807001537) do
+ActiveRecord::Schema.define(version: 20170808021613) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,6 +32,24 @@ ActiveRecord::Schema.define(version: 20170807001537) do
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+
+  create_table "holdings", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "portfolio_id"
+    t.integer  "security_id"
+    t.string   "symbol"
+    t.string   "asset_class"
+    t.decimal  "quantity"
+    t.date     "date_opened"
+    t.decimal  "cost_basis"
+    t.decimal  "avg_price"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "holdings", ["portfolio_id"], name: "index_holdings_on_portfolio_id"
+  add_index "holdings", ["security_id"], name: "index_holdings_on_security_id"
+  add_index "holdings", ["user_id"], name: "index_holdings_on_user_id"
 
   create_table "portfolios", force: :cascade do |t|
     t.integer  "user_id"
