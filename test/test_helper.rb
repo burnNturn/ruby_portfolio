3efe18859160031f5febb13a119f1870687e5d98
Jarrod Merryman <jarrodmerryman@Jarrods-MBP.fios-router.home>
2017-08-18 15:40:49 -0700

    new stuff

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,4 +7,7 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  RSpec.configure do |config|
+		config.include Devise::Test::ControllerHelpers, type: :controller
+  end
 end
