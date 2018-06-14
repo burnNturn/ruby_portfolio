04593593bc1b14b638dfab4dc64fe34da94d9290
Cory Nistler <c.nistler3@yahoo.com>
2018-06-14 23:36:19 +0000

    you can now upload csv of trans for a portfolio, added 2017 trans to test

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,43 +1,73 @@
 class Transaction < ActiveRecord::Base
+    require 'csv'
+    
     # before_save :get_portfolio_id
-    before_save :calculate_amount
+    before_save :calculate_amounts
     before_save :find_holding
     before_save :build_description
+    before_save :update_portfolio_balance
     
     belongs_to :user
     belongs_to :holding
     belongs_to :portfolio
     
+    def self.import(file, portfolio)
+        CSV.foreach(file.path, headers: true) do |row|
+            hash = row.to_hash
+            hash[:portfolio_id] = portfolio.id
+            Transaction.create! hash
+        end
+    end
+    
+    
     protected
     
-    def calculate_amount
-        self.amount = (self.quantity * self.price) + self.commission + self.fees
+    def calculate_amounts
+        if self.amount.nil?
+            self.amount = (self.quantity * self.price) + self.commission + self.fees
+        end
     end
     
     def find_holding()
-        byebug
         if self.holding.present?
             @holding = self.holding
-            @holding.sell_holding(self.quantity, self.amount)
+            if self.activity == 'Sell'
+                @holding.sell_holding(self.quantity, self.price)
+            elsif self.activity == 'Buy'
+                @holding.buy_holding(self.quantity. self.price)
+            end
         elsif Holding.where(user: self.user, portfolio: self.portfolio, symbol: self.symbol).exists?
             @holding = Holding.where(user: self.user, portfolio: self.portfolio, symbol: self.symbol).first
-            @holding.buy_holding(self.quantity, self.amount)
-        else
+            if self.activity == 'Buy'
+                @holding.buy_holding(self.quantity, self.price)
+            end
+        elsif !self.symbol.nil?
             @holding = Holding.create(user: self.user, portfolio: self.portfolio, 
                 symbol: self.symbol, quantity: self.quantity,
-                cost_basis: self.price, avg_price: self.amount)
+                cost_basis: self.price, avg_price: self.price)
+        else
+            @holding = nil
         end
         
         self.holding = @holding
     end
     
     def build_description
-        byebug
-       self.description = self.activity + " " + self.quantity.to_s + " shares of" + 
-        self.symbol + " @ " + self.price.to_s
+        if self.symbol.nil?
+            self.description = self.activity + " of " + self.amount.to_s
+        else
+            self.description = self.activity + " " + self.quantity.to_s + " shares of" + 
+            self.symbol + " @ " + self.price.to_s
+        end
     end
     
-    
+    def update_portfolio_balance
+        if self.activity == 'Buy' or self.activity == 'Withdraw'
+           self.portfolio.update_balance(self.amount * -1)
+        elsif self.activity == 'Sell' or self.activity == 'Contribution' or self.activity == 'Dividend'
+            self.portfolio.update_balance(self.amount)
+        end
+    end
     
     
     
